331b2100f4c67e75d0cd8116f3ad685979a5e03e
AgustinBraco <bracoagustin@gmail.com>
2023-05-30 22:40:44 -0300

    challenge-3-done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const port = 3000;
 import ProductManager from "./productManager.js";
 const productManager = new ProductManager("products");
 
-// Extended para uso de querys:
+// Middleware para uso de querys:
 app.use(express.urlencoded({ extended: true }));
 
 // Endpoint para mostrar los productos:
@@ -13,31 +13,28 @@ app.get("/products", async (req, res) => {
 	try {
 		const { limit } = req.query;
 		const products = await productManager.getProducts();
-
 		if (limit) {
-      // Limitar array según valor de 'limit':
+			// Limitar array según valor de 'limit':
 			const limitedProducts = products.slice(0, limit);
 			return res.json(limitedProducts);
-		} else {
-			return res.json(products);
-		};
+		}
+		res.json(products);
 	} catch (err) {
 		return res.status(500).json({ error: err.message });
-	};
+	}
 });
 
 // Endpoint para mostrar un producto según ID:
 app.get("/products/:pid", async (req, res) => {
 	try {
-    // Tomar 'id', convertirlo en número entero y buscar el producto:
+		// Tomar 'id', convertirlo en número entero y buscar el producto:
 		const { pid } = req.params;
 		const productId = parseInt(pid);
 		const product = await productManager.getProductById(productId);
-
 		return res.json(product);
 	} catch (err) {
 		return res.status(500).json({ error: err.message });
-	};
+	}
 });
 
 app.listen(port, () => {
